9da71560dc7513ab28f390c20571633feebd5e8d
pierrebrd <pierre.bordeau.79240@gmail.com>
2025-01-13 14:25:31 +0100

    fixed camera::ray_launcher

--- src/objects.hpp
+++ src/objects.hpp
@@ -2,12 +2,9 @@
 Ce fichier contiendra les classes concernant les rayons et leur suivi
 */
 
-
-
 #ifndef objectsHPP
 #define objectsHPP
 
-#include <vector>
 #include <cmath>
 #include <stdexcept>
 #include <iostream>
@@ -24,28 +21,8 @@ struct Point_3D
 
 };
 
-struct Vector_3D;
+struct Angles_Spherical; // We declare the class here to avoid circular dependencies
 
-struct Angles_Spherical {
-    // Represents the 2 angles between 2 vectors in 3D space using the spherical coordinates
-    float theta; // Angle with the z axis
-    float phi; // Angle with the x axis in the x-y plane
-
-    Angles_Spherical(float t, float p) : theta(t), phi(p) {}
-
-    Vector_3D to_vector(){
-        // Returns the vector corresponding to the angles in spherical coordinates
-        float x = std::sin(theta) * std::cos(phi);
-        float y = std::sin(theta) * std::sin(phi);
-        float z = std::cos(theta);
-        Vector_3D vector(x, y, z);
-        return vector;
-    }
-
-    void print() const {
-        std::cout << "Angles: (" << theta << ", " << phi << ")\n";
-    }
-};
 
 struct Vector_3D
 {
@@ -61,25 +38,51 @@ struct Vector_3D
         return std::sqrt(x * x + y * y + z * z);
     }
 
-    Angles_Spherical to_angles(){
-        // Returns the angles of the vector in spherical coordinates
-        auto r = norm();
-        if (r == 0){
-            throw std::invalid_argument("The vector is null, it has no angles");
-        }
-        float theta = std::acos(z/r); // Angle with the z axis
-        float phi = std::atan2(y, x); // Angle with the x axis in the x-y plane
-        Angles_Spherical angles(theta, phi);
-        return angles;
-    }
-
     void print() const {
         std::cout << "Vector: (" << x << ", " << y << ", " << z << ")\n";
     }
 
-
-
+    Angles_Spherical to_angles() const; // We declare the method here to avoid circular dependencies
 };
+
+
+struct Angles_Spherical
+{
+    // Represents the 2 angles between 2 vectors in 3D space using the spherical coordinates
+    float theta; // Angle with the z axis
+    float phi; // Angle with the x axis in the x-y plane
+
+    Angles_Spherical(float t, float p) : theta(t), phi(p) {}
+
+    void print() const {
+        std::cout << "Angles: (" << theta << ", " << phi << ")\n";
+    }
+
+    Vector_3D to_vector() const; // We declare the method here to avoid circular dependencies
+};
+
+// Methods of Vector_3D and Angles_Spherical :
+
+Vector_3D Angles_Spherical::to_vector() const {
+    // Returns the vector corresponding to the angles in spherical coordinates
+    float x = std::sin(theta) * std::cos(phi);
+    float y = std::sin(theta) * std::sin(phi);
+    float z = std::cos(theta);
+    Vector_3D vector(x, y, z);
+    return vector;
+}
+
+Angles_Spherical Vector_3D::to_angles() const {
+    // Returns the angles of the vector in spherical coordinates
+    auto r = norm();
+    if (r == 0) {
+        throw std::invalid_argument("The vector is null, it has no angles");
+    }
+    float theta = std::acos(z / r); // Angle with the z axis
+    float phi = std::atan2(y, x); // Angle with the x axis in the x-y plane
+    Angles_Spherical angles(theta, phi);
+    return angles;
+}
 
 
 
@@ -91,12 +94,12 @@ private:
 
 public:
     Ray(float xo, float yo, float zo, float xd, float yd, float zd) : origin(xo, yo, zo), direction(xd, yd, zd) {}
-    Ray(Point_3D ori, Vector_3D dir) : origin(ori),direction(dir){};
+    Ray(Point_3D ori, Vector_3D dir) : origin(ori), direction(dir) {};
 
     // Getters
-    Point_3D get_origin() const {return origin;}
-    Vector_3D get_direction() const {return direction;}
-    
+    Point_3D get_origin() const { return origin; }
+    Vector_3D get_direction() const { return direction; }
+
 
 
 };
@@ -112,35 +115,53 @@ private:
 
 public:
     // Constructor :
-    Camera(Point_3D pos = {0, 0, 0}, Vector_3D dir = {0, 1, 0}, unsigned f = 90, unsigned res_h = 400, unsigned res_w = 600) : position(pos), direction(dir), hfov(f), resolution_height(res_h), resolution_width(res_w) {}
-    // TODO : il faudrait vérifier que les valeurs de fov sont bien comprises entre 0 et 179 : genre si on a 179 en hfov mais que l'image est plus haute que large, il y aura un pb ???
+    Camera(Point_3D pos = { 0, 0, 0 }, Vector_3D dir = { 0, 1, 0 }, unsigned f = 90, unsigned res_h = 400, unsigned res_w = 600) : position(pos), direction(dir), hfov(f), resolution_height(res_h), resolution_width(res_w) {
+        if (hfov > 179) {
+            throw std::invalid_argument("horizontal fov must be less than 180 degrees");
+        }
+        if (resolution_height == 0 || resolution_width == 0) {
+            throw std::invalid_argument("resolution can not be 0");
+        }
+        if (hfov * resolution_height / resolution_width > 179) {
+            throw std::invalid_argument("vertical fov must be less than 180 degrees");
+        }
+        if (direction.norm() == 0) {
+            throw std::invalid_argument("The direction vector of the camera can not be null");
+        }
+    }
+
     // Destructor :
-    ~Camera();
+    ~Camera() {};
 
     // Getters :
-    Point_3D get_position() const {return position;}
-    Vector_3D get_direction() const {return direction;}
-    unsigned get_hfov() const {return hfov;}
-    unsigned get_resolution_height() const {return resolution_height;}
-    unsigned get_resolution_width() const {return resolution_width;}
-    
+    Point_3D get_position() const { return position; }
+    Vector_3D get_direction() const { return direction; }
+    unsigned get_hfov() const { return hfov; }
+    unsigned get_resolution_height() const { return resolution_height; }
+    unsigned get_resolution_width() const { return resolution_width; }
 
-    Ray ray_launcher(unsigned pixel_height,unsigned pixel_width){
-        // Launches a Ray from the camera to the given pixel coordinates
+
+    Ray ray_launcher(unsigned pixel_height, unsigned pixel_width) {
+        // Launches a Ray from the camera to the given pixel coordinates. Coordinates start at 0, end at resolution_height-1 and resolution_width-1
         // TODO : Check that the pixel coordinates are within the resolution, otherwise return an exception
-        
+
         // The coordinates starts on the bottom left corner
         Point_3D origin = Camera::position;
         // camera::direction defines the center of the image, when pixel_height = resolution_height/2 and pixel_width = resolution_width/2
         // We consider that the camera is not rotated along the y axis (compared to the default viewing direction along the y axis)
-        // Pixel 0,0 is the bottom left corner, so the direction is
-        float horizontal_angle = (hfov * (pixel_width - resolution_width/2) / (resolution_width/2)) * M_PI / 180.0; // In radians
-        float vfov = hfov * resolution_height / resolution_width; // Vertical field of view in degrees
-        float vertical_angle = (vfov * (pixel_height - resolution_height/2) / (resolution_height/2)) * M_PI / 180.0; // In radians
-        
+
+        // We need to cast the unsigned to floats to avoid integer division
+        float hfov_f = static_cast<float>(hfov);
+        float resolution_height_f = static_cast<float>(resolution_height);
+        float resolution_width_f = static_cast<float>(resolution_width);
+        float vfov_f = hfov_f * resolution_height_f / resolution_width_f; // Vertical field of view in degrees
+
+        float horizontal_angle = ((hfov_f / 2) * (pixel_width - resolution_width_f / 2 + 0.5) / (resolution_width / 2)) * M_PI / 180.0; // In radians. The second resolution_width/2 is integer division
+        float vertical_angle = ((vfov_f / 2) * (pixel_height - resolution_height_f / 2 + 0.5) / (resolution_height / 2)) * M_PI / 180.0; // In radians
+
         auto camera_direction_angles = direction.to_angles();
         float theta = camera_direction_angles.theta - vertical_angle;
-        float phi = camera_direction_angles.phi + horizontal_angle;
+        float phi = camera_direction_angles.phi - horizontal_angle;
 
         Vector_3D ray_direction = Angles_Spherical(theta, phi).to_vector();
         return Ray(origin, ray_direction);
@@ -150,9 +171,9 @@ public:
 class Object
 {
     class Shape
+    {};
+    class Texture
     {
-    };
-    class Texture{
 
     };
 };
@@ -162,9 +183,9 @@ class Sphere : public Object
     Point_3D center;
     float radius;
 
-    public:
-        // Constructor
-        Sphere(Point_3D c, float r): center(c), radius(r){};
+public:
+    // Constructor
+    Sphere(Point_3D c, float r) : center(c), radius(r) {};
 
 
 };
@@ -175,15 +196,15 @@ class Plane : public Object
     Vector_3D vector1;
     Vector_3D vector2;
 
-    public:
-        // Constructor
-        Plane(Point_3D  o, Vector_3D v1, Vector_3D v2): origin(o), vector1(v1), vector2(v2){};    
+public:
+    // Constructor
+    Plane(Point_3D  o, Vector_3D v1, Vector_3D v2) : origin(o), vector1(v1), vector2(v2) {};
 
-        // Methods
-        Point_3D find_intersection(Ray myRay) const{
-            myRay.get_direction();
-            Vector_3D normal= Vector_3D()
-        }
+    //Methods
+    Point_3D find_intersection(Ray myRay) const {
+        myRay.get_direction();
+        Vector_3D normal = Vector_3D();
+    }
 };
 
 #endif
--- src/test_vectors.cpp
+++ src/test_vectors.cpp
@@ -1,10 +1,14 @@
 #include "objects.hpp"
 
-int main(){
-    auto cam = Camera();
-    auto direction = cam.get_direction();
-    auto angles = direction.to_angles();
-    direction.print();
-    angles.print();
+int main() {
+    Camera cam(Point_3D(0, 0, 0), Vector_3D(0, 1, 0), 90, 5, 7);
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 7; j++) {
+            Ray ray = cam.ray_launcher(i, j);
+            std::cout << "Ray " << i << " " << j << std::endl;
+            ray.get_direction().print();
+            ray.get_direction().to_angles().print();
+        }
+    }
     return 0;
 }
